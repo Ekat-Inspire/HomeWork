61c1c13504bf7cf884df78ee0abe49b54c2f6971
Ekat-Inspire <inspire-07@mail.ru>
2021-09-01 00:41:48 +0500

    Новая попытка

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+            {
+                
+            Console.WriteLine("Введите имя пользователя:");
+                var name = Console.ReadLine();
+
+                
+            
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Now}"); // не могу отправить в git
+            }
+    }
+}
