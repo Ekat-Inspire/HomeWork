d7cc31aa298a9aedc2efd73d752a68df73ce68b7
Ekat-Inspire <inspire-07@mail.ru>
2021-09-04 17:53:50 +0500

    Добавено домашнее задание к Урок1

---
+++ Lesson1Project1/Lesson1Project1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя пользователя:");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Привет {name}, сегодня {DateTime.Now}"); 
+    }
+}
