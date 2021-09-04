b887e8b3154e9c9356f8e13d46de8b01d1a121bc
Ekat-Inspire <inspire-07@mail.ru>
2021-09-04 16:33:11 +0500

    Загружено домашнее задание к Уроку 2

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Укажите минимальную температуру за сутки:");
+            var min = Console.ReadLine();
+            int a = Convert.ToInt32(min);
+
+            Console.WriteLine("Укажите максимальную температуру за сутки:");
+            var max = Console.ReadLine();
+            int b = Convert.ToInt32(max);
+
+            var x = (a + b) / 2;
+
+            Console.WriteLine($"Среднесуточная температура: {x}");
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
