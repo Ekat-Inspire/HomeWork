d82589c605ae2f837fb4ab531fe503c4ad2653cd
Ekat-Inspire <inspire-07@mail.ru>
2021-09-06 20:20:54 +0500

    Добавлено домашнее задание к Урок3

---
+++ Lesson3Project1/Lesson3Project1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson3Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int next_string = 0;
+            int[,] array = new int[3,3];
+                        
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = next_string + 1;
+
+
+                    Console.WriteLine($"{new string(' ', next_string)}{array[i, j]}");
+                    next_string++;
+                }
+            }
+        }
+    }
+}
---
+++ Lesson3Project3/Lesson3Project3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson3Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите тект:");
+
+            string a = Console.ReadLine();
+            
+                for (int i = a.Length - 1; i >= 0; i--)
+                    Console.Write(a[i]);
+                            
+
+
+
+            
+
+        }
+    }
+}
