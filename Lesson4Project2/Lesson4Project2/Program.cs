0ebbfe7b8f4c4a991b99760923c7f9f7944f09dc
Ekat-Inspire <inspire-07@mail.ru>
2021-09-09 20:15:48 +0500

    Добавлено Домашнее Задание к Урок4

---
+++ Lesson4Project2/Lesson4Project2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson4Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите набор чисел, разделенных пробелом:");
+            string a  = Console.ReadLine();
+            int x = Convert.ToInt32(a);
+        
+            int sum = x.Sum();
+            Console.WriteLine("Сумма элементов: " + sum);
+
+            // Решение задачи не получилось: 1) не могу понять, как строку введеную, перевестив масств int []
+
+          }
+    }
+}
