dcb5d41e4c8edd81af4476d11f9854c3b7697ca8
Ekat-Inspire <inspire-07@mail.ru>
2021-09-12 18:43:05 +0500

    Добавлено Домашне задание к Урок5

---
+++ Lesson5Project2/Lesson5Project2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text; 
+
+
+namespace Lesson5Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+           
+            File.WriteAllLines(filename, new[] { DateTime.Now.ToString(), "Перед началом этого текста, Вы должны увидеть текущую дату и время :) " });
+
+            string fileText = File.ReadAllText(filename);
+            Console.WriteLine(fileText);
+        }
+    }
+}
---
+++ Lesson5Project3/Lesson5Project3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Text;
+using System;
+
+
+namespace Lesson5Project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный набор числе (0..255)"); // и записать их в бинарный файл
+
+            var my = Console.ReadLine();
+                        
+            
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(new FileStream("my.bin", FileMode.OpenOrCreate), my);
+
+
+        }
+    }
+}
